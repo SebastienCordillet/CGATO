356361df235de10e5019190c90fba74d334cad75
SebastienCordillet <39163740+SebastienCordillet@users.noreply.github.com>
2021-12-30 22:59:46 +0100

    first_commit
    vsto package initialisation

---
+++ CGATO_addin/Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace CGATO_addin
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var wordApp = new Word
+        }
+    }
+}
